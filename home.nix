3262e395ed28415feb39cb84f70b36965b7afc7c
Armin Jöllenbeck <armin@joellenbeck.net>
2024-12-11 00:06:01 +0100

    Initial commit
    Configure home-manager just enough to use git for pushing to GitHub.

---
+++ home.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  home.stateVersion = "24.11"; # Please check the Home Manager release notes before changing.  
+  programs.home-manager.enable = true;
+
+  home.username = "ajoellenbeck";
+  home.homeDirectory = "/home/ajoellenbeck";
+
+  home.packages = [
+    pkgs.hello
+
+    (pkgs.writeShellScriptBin "my-hello" ''
+      echo "Hello, ${config.home.username}!"
+    '')
+  ];
+
+  programs.gh = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      user.name = "Armin Jöllenbeck";
+      user.email = "armin@joellenbeck.net";
+      init.defaultBranch = "main";
+    };
+  };
+
+  home.file = {
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+}
