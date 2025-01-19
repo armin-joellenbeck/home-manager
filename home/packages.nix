cce39c417a310413b763c24afa3351c7b75e8107
Armin Jöllenbck <armin@joellenbeck.net>
2025-01-19 16:27:04 +0100

    Consolidate Rust installation

--- home/packages.nix
+++ home/packages.nix
@@ -3,11 +3,13 @@
 {
   home.packages = [
     pkgs.cargo
+    pkgs.clippy
     pkgs.curl
     pkgs.gcc
     pkgs.hello
     pkgs.live-server
     pkgs.rust-analyzer
+    pkgs.rustc
     pkgs.tldr
     pkgs.unzip
     pkgs.wget
