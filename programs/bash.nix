27a4eaa208d354b6669c4b6e955d9719c79c4597
Armin Jöllenbck <armin@joellenbeck.net>
2025-01-20 12:53:47 +0100

    Add RUST_SRC_PATH environment variable

--- programs/bash.nix
+++ programs/bash.nix
@@ -7,5 +7,8 @@
       ll = "ls -al";
       ls = "ls --color=auto --group-directories-first -v";
     };
+    sessionVariables = {
+    	RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
   };
 }
